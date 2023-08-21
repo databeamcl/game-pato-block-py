8c3613f10bd9f12657108aedeb1dd51322a1bef6
Alvaro Fuentes <alvarofue@gmail.com>
2023-08-21 15:36:42 -0400

    fix

--- main.py
+++ main.py
@@ -17,6 +17,13 @@ screen = pygame.display.set_mode((1261, 663))
 
 blocks_size = 5
 blocks = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+# crear una matriz de 19 x 19 con valor 0  
+block_matrix = [[1 for x in range(19)] for y in range(10)]
+
+print(block_matrix)
+
+
 block_start = 0
 block_end = 18
 block_cursor = 0
@@ -38,11 +45,12 @@ def menu_game():
 
 def update():
     # sleep 1 second
-    move_blocks()
+    # move_blocks()
     pygame.time.delay(500)
 
 def render():
-    draw_blocks()
+    draw_blocks_matrix()
+    # draw_blocks()
     pygame.display.flip() # actualiza la pantalla
     # clock.tick(60) # limita a 30 fps
 
@@ -63,6 +71,16 @@ def draw_blocks():
     for i in range(0, 19): # 0 - 18
       if blocks[i] == 1:
             screen.blit(block, (320 + (i * 32), 600))
+
+def draw_blocks_matrix():
+    # for row in range(len(blocks)):
+    #     for col in range(len(blocks[row])):
+    #         if blocks[row][col] == 1:
+    #             screen.blit(block, (320 + (col * 32), 600 + (row * 32)))
+    # for i in range(len(blocks_matrix)) :
+    #     if blocks[i] == 1:
+    #         screen.blit(block, (320 + (i * 32), 600))
+    pass
 
 def move_blocks():
     global block_cursor
