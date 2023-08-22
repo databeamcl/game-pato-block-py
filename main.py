c830b6c4300916a085cc6f05748bd4274408e20d
Alvaro Fuentes <alvarofue@gmail.com>
2023-08-21 20:30:41 -0400

    fix

--- main.py
+++ main.py
@@ -18,15 +18,12 @@ screen = pygame.display.set_mode((1261, 663))
 
 blocks_size = 5
 
+# menu
+menu = 0
+
 # crear una matriz de 19 x 19 con valor 0  
 block_matrix = [[random.randint(0, 1) for x in range(19)] for y in range(18)]
 
-# print(block_matrix)
-
-
-# block_start = 0
-# block_end = 18
-# block_cursor = 0
 
 def menu_main():
     # Sonido.main_sound(pygame)
@@ -34,7 +31,7 @@ def menu_main():
     pygame.display.set_caption("PatoBlock Game (c)")
     screen.blit(background, (0, 0))
     font = pygame.font.Font(None, 50)
-    txtstart = font.render("1 - Start", True, (255, 255, 0))
+    txtstart = font.render("1 - Star Game", True, (255, 255, 0))
     txtexit = font.render("2 - Exit", True, (255, 0, 255))
     screen.blit(txtstart, (550, 150))
     screen.blit(txtexit, (550, 190))
@@ -44,37 +41,38 @@ def menu_game():
     screen.blit(backblock, (318, 54))
 
 def update():
-    # sleep 1 second
-    # move_blocks()
+    # sleep x seconds
     pygame.time.delay(100)
-    
+    #pass
 
 def render():
-    draw_blocks_matrix()
-    # draw_blocks()
-    pygame.display.flip() # actualiza la pantalla
-    # clock.tick(60) # limita a 30 fps
-    # clean screen
-    #screen.blit(background, (0, 0))
-    screen.blit(backblock, (318, 54))
+    if menu == 0:
+        menu_main()
+    elif menu == 1:
+        menu_game()
+        draw_blocks_matrix()
+    # actualiza la pantalla
+    pygame.display.flip()
+    # screen.blit(backblock, (318, 54))
 
 def keyboardcontroller(running):
+    global menu
+    if menu == 0:
+        menu_main()
+    elif menu == 1:
+        menu_game()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_2:
-                running = False
+                # running = False
+                menu = 0
             elif event.key == pygame.K_1:
-                menu_game()
+                menu = 1
             elif event.key == pygame.K_ESCAPE:
-                menu_main()
+                menu = 0
     return running
-
-# def draw_blocks():
-#     for i in range(0, 19): # 0 - 18
-#       if blocks[i] == 1:
-#             screen.blit(block, (320 + (i * 32), 600))
 
 def draw_blocks_matrix():
     block_matrix = [[random.randint(0, 1) for x in range(19)] for y in range(18)]
@@ -87,7 +85,6 @@ def move_blocks():
     global block_cursor
     blocks[block_cursor] = 1
     block_cursor += 1
-    # print(blocks)
 
 def move_blocks_right():
     pass
@@ -102,8 +99,6 @@ def cal_pos_x(x):
 def cal_pos_y(y):
     y = 600 - (y * 32)
     return y
-
-
 
 menu_main()
 
