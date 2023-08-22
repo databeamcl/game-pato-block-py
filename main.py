8668fdb62cb4038c1b46dae57dcfc61fe6d06e71
Alvaro Fuentes <alvarofue@gmail.com>
2023-08-22 13:13:06 -0400

    fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from util.sonido import Sonido
+from util.sound import Sound
 import random
 
 pygame.init()
@@ -15,18 +15,16 @@ clock = pygame.time.Clock()
 running = True
 screen = pygame.display.set_mode((1261, 663))
 # screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-
+# set size of blocks
 blocks_size = 5
-
-# menu
+# set menu initial
 menu = 0
-
-# crear una matriz de 19 x 19 con valor 0  
+# set matrix of blocks
 block_matrix = [[random.randint(0, 1) for x in range(19)] for y in range(18)]
 
 
-def menu_main():
-    # Sonido.main_sound(pygame)
+def screen_main():
+    # Sound.main_sound(pygame)
     pygame.display.set_icon(icon)
     pygame.display.set_caption("PatoBlock Game (c)")
     screen.blit(background, (0, 0))
@@ -36,38 +34,37 @@ def menu_main():
     screen.blit(txtstart, (550, 150))
     screen.blit(txtexit, (550, 190))
 
-def menu_game():
-    # Sonido.game_sound(pygame)
+def screen_game():
+    # Sound.game_sound(pygame)
     screen.blit(backblock, (318, 54))
+    draw_blocks_matrix()
 
 def update():
-    # sleep x seconds
-    pygame.time.delay(100)
-    #pass
+    pass
+    
 
 def render():
     if menu == 0:
-        menu_main()
+        screen_main()
     elif menu == 1:
-        menu_game()
-        draw_blocks_matrix()
-    # actualiza la pantalla
+        screen_game()
+    # update display
+    pygame.time.delay(100)
     pygame.display.flip()
-    # screen.blit(backblock, (318, 54))
 
-def keyboardcontroller(running):
+
+def keyboardcontroller():
+    global running
     global menu
-    if menu == 0:
-        menu_main()
-    elif menu == 1:
-        menu_game()
+    # get events of keyboard
     for event in pygame.event.get():
+        # when close window
         if event.type == pygame.QUIT:
             running = False
+        # when key is pressed
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_2:
-                # running = False
-                menu = 0
+                running = False
             elif event.key == pygame.K_1:
                 menu = 1
             elif event.key == pygame.K_ESCAPE:
@@ -100,10 +97,8 @@ def cal_pos_y(y):
     y = 600 - (y * 32)
     return y
 
-menu_main()
-
 while running:
-    running = keyboardcontroller(running)
+    keyboardcontroller()
     update()
     render()
 
--- util/sound.py
+++ util/sound.py
@@ -1,19 +1,19 @@
-class Sonido:
+class Sound:
 
     def main_sound(pygame):
-        # cargar un mid
+        # load mid
         pygame.mixer.music.load("./resources/musica.mid")
-        # cambair el volumen
-        pygame.mixer.music.set_volume(0.5)
-        # reproducir el mid una vez
+        # change volume
+        pygame.mixer.music.set_volume(1.0)
+        # play mid once
         pygame.mixer.music.play(1)
         #pygame.mixer.music.play(-1)
         
     def game_sound(pygame):
-        # cargar un mid
+        # load mid
         pygame.mixer.music.load("./resources/dk-island-swing.mid")
-        # cambair el volumen
-        pygame.mixer.music.set_volume(0.5)
-        # reproducir el mid una vez
+        # change volume
+        pygame.mixer.music.set_volume(1.0)
+        # play mid once
         pygame.mixer.music.play(1)
         #pygame.mixer.music.play(-1)
