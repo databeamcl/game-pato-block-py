a8ed19dbab5d93a93c06c0533d1862e013e537ec
Alvaro Fuentes <alvarofue@gmail.com>
2023-08-22 14:13:37 -0400

    fix

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import random
 pygame.init()
 
 # -------------------------------------------- load resources
+
 icon = pygame.image.load("./resources/icon.png")
 background = pygame.image.load("./resources/fondoarcade.png")
 backblock = pygame.image.load("./resources/back.png")
 block = pygame.image.load("./resources/block.png")
 
 # -------------------------------------------- set up
+
+pygame.display.set_icon(icon)
+pygame.display.set_caption("PatoBlock Game (c)")
 clock = pygame.time.Clock()
 running = True
 screen = pygame.display.set_mode((1261, 663))
@@ -24,10 +28,16 @@ block_matrix = [[random.randint(0, 1) for x in range(19)] for y in range(18)]
 
 # -------------------------------------------- screen stage
 
+def cal_pos_x(x):
+    x = 320 + (x * 32)
+    return x
+
+def cal_pos_y(y):
+    y = 600 - (y * 32)
+    return y
+
 def screen_main():
     # Sound.main_sound(pygame)
-    pygame.display.set_icon(icon)
-    pygame.display.set_caption("PatoBlock Game (c)")
     screen.blit(background, (0, 0))
     font = pygame.font.Font(None, 50)
     txtstart = font.render("1 - Star Game", True, (255, 255, 0))
@@ -47,30 +57,12 @@ def draw_blocks_matrix():
             if block_matrix[x][y] == 1:
                 screen.blit(block, (cal_pos_x(y), cal_pos_y(x)))
 
-def move_blocks():
-    global block_cursor
-    blocks[block_cursor] = 1
-    block_cursor += 1
-
-def move_blocks_right():
-    pass
-
-def move_blocks_left():
-    pass
-
-def cal_pos_x(x):
-    x = 320 + (x * 32)
-    return x
-
-def cal_pos_y(y):
-    y = 600 - (y * 32)
-    return y
-
 # -------------------------------------------- render stage
 
 def keyboardcontroller():
     global running
     global menu
+
     # get events of keyboard
     for event in pygame.event.get():
         # when close window
@@ -84,7 +76,6 @@ def keyboardcontroller():
                 menu = 1
             elif event.key == pygame.K_ESCAPE:
                 menu = 0
-    return running
 
 def update():
     pass
